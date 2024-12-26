c8c3d4a7de83d34847106e9b10b695cdd284ad06
Emre Can Palaz <palazemre1@gmail.com>
2024-12-26 19:15:15 +0300

    vercel

--- frontend/src/services/http-common.jsx
+++ frontend/src/services/http-common.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "http://localhost:3000/api",
+  //baseURL: "http://localhost:3000/api",
+  baseURL:"mekanbul-main.vercel.app",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
