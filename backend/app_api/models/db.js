d72fbb865d9399bc9e67ee0a8588c74ebdae6d74
Emre Can Palaz <palazemre1@gmail.com>
2024-12-26 19:07:47 +0300

    cloud-eklendi

--- backend/app_api/models/db.js
+++ backend/app_api/models/db.js
@@ -1,7 +1,8 @@
 var mongoose=require('mongoose');
 //Cloud mongodb için aşağıdaki adresi cloud adresiyle değiştirin.
 //var dbURI="mongodb+srv://sinan:sinan@cluster0.4or9sis.mongodb.net/mekanbul"
-var dbURI="mongodb://localhost/mekanbul";
+//var dbURI="mongodb://localhost/mekanbul";
+var dbURI="mongodb+srv://palazemre1:Emre1234@cluster1.bco8b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1"
 mongoose.connect(dbURI);
 mongoose.connection.on("connected",function(){
     console.log(dbURI+" adresine bağlandı");
